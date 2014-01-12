2f334180f4492914bb5f1974baf4267b40be2838
srossd <rossd97@gmail.com>
2014-01-12 01:21:02 -0500

    simple attack/pastr strategy

--- Battlecode2014/teams/team135/RobotPlayer.java
+++ Battlecode2014/teams/team135/RobotPlayer.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 import battlecode.common.Direction;
 import battlecode.common.GameActionException;
+import battlecode.common.MapLocation;
 import battlecode.common.Robot;
 import battlecode.common.RobotController;
 import battlecode.common.RobotInfo;
@@ -12,23 +13,29 @@ import battlecode.common.TerrainTile;
 
 public class RobotPlayer {
 	static Random rand;
+	static Direction[] directions, roads;
 
 	public static void run(RobotController rc) {
 		rand = new Random();
-		Direction[] directions = { Direction.NORTH, Direction.NORTH_EAST,
+		directions = new Direction[] { Direction.NORTH, Direction.NORTH_EAST,
 				Direction.EAST, Direction.SOUTH_EAST, Direction.SOUTH,
 				Direction.SOUTH_WEST, Direction.WEST, Direction.NORTH_WEST };
+		try {
+			roads = roads(rc, rc.senseHQLocation());
+		} catch (GameActionException e) {
+			System.out.println("roads exception");
+		}
 
 		while (true) {
 			if (rc.getType() == RobotType.HQ) {
 				try {
 					// Check if a robot is spawnable and spawn one if it is
 					if (rc.isActive() && rc.senseRobotCount() < 25) {
-						Direction toEnemy = rc.getLocation().directionTo(
-								rc.senseEnemyHQLocation());
+						int r = rand.nextInt(roads.length);
+						Direction toRoad = roads[r];
 						if (rc.senseObjectAtLocation(rc.getLocation().add(
-								toEnemy)) == null) {
-							rc.spawn(toEnemy);
+								toRoad)) == null) {
+							rc.spawn(toRoad);
 						}
 					}
 				} catch (Exception e) {
@@ -39,36 +46,10 @@ public class RobotPlayer {
 			if (rc.getType() == RobotType.SOLDIER) {
 				try {
 					if (rc.isActive()) {
-						int action = (rc.getRobot().getID() * rand.nextInt(101) + 50) % 101;
-						// Construct a PASTR
-						if (action < 1
-								&& rc.getLocation().distanceSquaredTo(
-										rc.senseHQLocation()) > 2) {
-							rc.construct(RobotType.PASTR);
-							// Attack a random nearby enemy
-						} else if (action < 30) {
-							Robot[] nearbyEnemies = rc.senseNearbyGameObjects(
-									Robot.class, 10, rc.getTeam().opponent());
-							if (nearbyEnemies.length > 0) {
-								RobotInfo robotInfo = rc
-										.senseRobotInfo(nearbyEnemies[0]);
-								rc.attackSquare(robotInfo.location);
-							}
-							// Move in a random direction
-						} else if (action < 80) {
-							Direction moveDirection = directions[rand
-									.nextInt(8)];
-							if (rc.canMove(moveDirection)) {
-								rc.move(moveDirection);
-							}
-							// Sneak towards the enemy
-						} else {
-							Direction toEnemy = rc.getLocation().directionTo(
-									rc.senseEnemyHQLocation());
-							if (rc.canMove(toEnemy)) {
-								rc.sneak(toEnemy);
-							}
-						}
+						if (rc.getRobot().getID() % 2 == 0)
+							attackBot(rc);
+						else
+							pastrBot(rc);
 					}
 				} catch (Exception e) {
 					System.out.println("Soldier Exception");
@@ -79,21 +60,95 @@ public class RobotPlayer {
 		}
 	}
 
-	public static void seekOpponentHQ(RobotController rc)
-			throws GameActionException {
-		Direction toOpponent = rc.getLocation().directionTo(
-				rc.senseEnemyHQLocation());
-		int index = 0;
-		Direction[] directions = Direction.values();
-		while (!directions[index].equals(toOpponent))
-			index++;
-		for (int i = index; i < index + 8; i++) {
-			int ii = i % 8;
-			if (rc.canMove(directions[ii])
-					&& rc.senseTerrainTile(rc.getLocation().add(directions[ii])) == TerrainTile.ROAD) {
-				rc.sneak(directions[ii]);
-				break;
-			}
+	public static void attackBot(RobotController rc) throws GameActionException {
+		Robot[] nearbyEnemies = rc.senseNearbyGameObjects(Robot.class, 10, rc
+				.getTeam().opponent());
+		MapLocation target = rc.senseEnemyHQLocation();
+		if (rc.getLocation().distanceSquaredTo(rc.senseEnemyHQLocation()) < 200
+				&& nearbyEnemies.length > 0
+				&& rc.senseRobotInfo(nearbyEnemies[0]).type != RobotType.HQ)
+			target = rc.senseRobotInfo(nearbyEnemies[0]).location;
+		if (rc.getLocation().distanceSquaredTo(rc.senseEnemyHQLocation()) < 200
+				&& nearbyEnemies.length > 1
+				&& rc.senseRobotInfo(nearbyEnemies[1]).type != RobotType.HQ)
+			target = rc.senseRobotInfo(nearbyEnemies[1]).location;
+		Direction toOpponent = rc.getLocation().directionTo(target);
+		if (nearbyEnemies.length > 0
+				&& rc.senseRobotInfo(nearbyEnemies[0]).type != RobotType.HQ) {
+			RobotInfo robotInfo = rc.senseRobotInfo(nearbyEnemies[0]);
+			rc.attackSquare(robotInfo.location);
 		}
+		int i = 0;
+		if (rc.canMove(toOpponent)
+				&& rc.senseTerrainTile(rc.getLocation().add(toOpponent)) == TerrainTile.ROAD) {
+			rc.sneak(toOpponent);
+			return;
+		}
+		i++;
+		i = 0;
+		for (Direction d = toOpponent; i < 8; d = d.rotateLeft()) {
+			if (rc.canMove(d)) {
+				rc.sneak(d);
+				return;
+			}
+			i++;
+		}
+
+	}
+
+	public static void pastrBot(RobotController rc) throws GameActionException {
+		if (rand.nextInt(50) < 1
+				&& rc.senseCowsAtLocation(rc.getLocation()) > 100)
+			rc.construct(RobotType.PASTR);
+		double[] cows = new double[8];
+		Direction[] directions = Direction.values();
+		for (int i = 0; i < 8; i++)
+			cows[i] = rc.senseCowsAtLocation(rc.getLocation()
+					.add(directions[i]));
+		int index = maxIndex(cows);
+		if (index < 0) {
+			Direction[] roads = roads(rc, rc.getLocation());
+			int r = rand.nextInt(roads.length);
+			Direction toRoad = roads[r];
+			rc.sneak(toRoad);
+			return;
+		}
+		if (rc.senseCowsAtLocation(rc.getLocation()) > Math.max(cows[index],
+				100))
+			rc.construct(RobotType.PASTR);
+		else if (rc.canMove(directions[index]))
+			rc.sneak(directions[index]);
+		else {
+			Direction d = directions[rand.nextInt(8)];
+			if (rc.canMove(d))
+				rc.move(directions[rand.nextInt(8)]);
+		}
+	}
+
+	public static Direction[] roads(RobotController rc, MapLocation loc)
+			throws GameActionException {
+		int n = 0;
+		for (Direction d : Direction.values()) {
+			if (rc.senseTerrainTile(loc.add(d)).equals(TerrainTile.ROAD))
+				n++;
+		}
+		if (n == 0)
+			return directions;
+		Direction[] roads = new Direction[n];
+		int i = 0;
+		for (Direction d : Direction.values()) {
+			if (rc.senseTerrainTile(loc.add(d)).equals(TerrainTile.ROAD))
+				roads[i++] = d;
+		}
+		return roads;
+	}
+
+	public static int maxIndex(double[] xs) {
+		int index = -1;
+		for (int i = 0; i < xs.length; i++)
+			if ((index == -1 && xs[i] > xs[0])
+					|| (index != -1 && xs[i] > xs[index]))
+				index = i;
+		return index;
 	}
 }
