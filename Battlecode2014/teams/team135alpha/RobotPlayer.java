c2c2426106622d33a50af3a436c1a3a9b8d11c9a
Risheek Rajolu <risheekrr@gmail.com>
2014-01-26 06:25:16 -0500

    edited team135alpha: robots navigate, create pastrs, and guard pastrs

--- Battlecode2014/teams/team135alpha/RobotPlayer.java
+++ Battlecode2014/teams/team135alpha/RobotPlayer.java
@@ -1,17 +1,28 @@
 package team135alpha;
 
 import battlecode.common.Direction;
+import battlecode.common.MapLocation;
 import battlecode.common.Robot;
 import battlecode.common.RobotController;
 import battlecode.common.RobotType;
+import battlecode.common.TerrainTile;
 
 public class RobotPlayer {
 
 	static RobotController rc;
-	static Direction[] directions = Direction.values();
+	static Direction[] directions = new Direction[] { Direction.NORTH,
+			Direction.NORTH_EAST, Direction.EAST, Direction.SOUTH_EAST,
+			Direction.SOUTH, Direction.SOUTH_WEST, Direction.WEST,
+			Direction.NORTH_WEST };
 
 	/* channel numbers */
 	static int releaseAttackChannel = 0;
+
+	/* TODO replace temp */
+	static int[] cornerChannels = new int[] { 1, 2, 3, 4 }; // corner pastr,
+															// guard1, guard2,
+															// noise
+	static MapLocation[] corners;
 
 	public static void run(RobotController rc) {
 
@@ -39,7 +50,6 @@ public class RobotPlayer {
 	private static void runHQ() {
 		while (true) {
 			try {
-				boolean surrounded = true;
 				for (Direction d : directions) {
 					while (!rc.isActive())
 						rc.yield();
@@ -55,19 +65,17 @@ public class RobotPlayer {
 						if (rc.readBroadcast(releaseAttackChannel) != 0) {
 							rc.broadcast(releaseAttackChannel, 0);
 						}
-						
+						rc.spawn(d);
 					}
-					surrounded = false;
-					break;
 				}
-
-				if (surrounded) {
+				// TODO REMOVE IF-STATEMENT AFTER ADDING CODE FOR ROBOTS
+				// RESPONDING TO BROADCAST
+				if (rc.readBroadcast(releaseAttackChannel) != 1) {
 					rc.broadcast(releaseAttackChannel, 1);
-				} else {
-
 				}
+				rc.yield();
 			} catch (Exception e) {
-				System.out.print("HQ Exception");
+				System.out.println("HQ Exception");
 				e.printStackTrace();
 			}
 		}
@@ -75,7 +83,102 @@ public class RobotPlayer {
 	}
 
 	private static void runSoldier() {
+		boolean docked = true;
+		while (true) {
+			try {
+				while (!rc.isActive())
+					rc.yield();
+				for (Robot[] nearbyEnemies = rc.senseNearbyGameObjects(
+						Robot.class, 10, rc.getTeam().opponent()); nearbyEnemies.length > 0; nearbyEnemies = rc
+						.senseNearbyGameObjects(Robot.class, 10, rc.getTeam()
+								.opponent())) {
+					rc.attackSquare(rc.senseRobotInfo(nearbyEnemies[0]).location);
+					while (!rc.isActive())
+						rc.yield();
+				}
+				int width = rc.getMapWidth();
+				int height = rc.getMapHeight();
+				corners = new MapLocation[] {
+						new MapLocation(width - 3, height - 3),
+						new MapLocation(width - 3, 2),
+						new MapLocation(2, height - 3), new MapLocation(2, 2) };
+				MapLocation center = new MapLocation(width / 2, height / 2);
+				for (int i = 0; i < 4; i++) {
+					TerrainTile tt = rc.senseTerrainTile(corners[i]);
+					if (tt != TerrainTile.NORMAL && tt != TerrainTile.ROAD)
+						corners[i] = corners[i].add(corners[i]
+								.directionTo(center));
+				}
+				if (docked) {
+					if (rc.readBroadcast(releaseAttackChannel) == 1) {
+						for (int i = 0; i < cornerChannels.length; i++) {
+							int corner = rc.readBroadcast(cornerChannels[i]);
+							if (corner < 4) {
+								rc.broadcast(cornerChannels[i], corner + 1);
+								navigate(corners[corner]);
+								if (i == 0) {
+									while (!rc.isActive())
+										rc.yield();
+									rc.construct(RobotType.PASTR);
+									while (true)
+										rc.yield();
+								} else if (i == 4) {
+									rc.construct(RobotType.NOISETOWER);
+									while (true)
+										rc.yield();
+								} else {
 
+								}
+
+								break;
+							}
+						}
+						docked = false;
+					}
+				}
+
+			} catch (Exception e) {
+				System.out.println("SOLDIER Exception");
+				e.printStackTrace();
+			}
+
+		}
+	}
+
+	private static void navigate(MapLocation target) {
+		System.out.println(target);
+		try {
+			int dirTo = rc.getLocation().directionTo(target).ordinal();
+			int from = rc.getLocation().directionTo(rc.senseHQLocation())
+					.ordinal();
+			for (int distTo = rc.getLocation().distanceSquaredTo(target); distTo > 1
+					|| distTo == 1 && rc.canMove(directions[dirTo]); distTo = rc
+					.getLocation().distanceSquaredTo(target)) {
+				while (!rc.isActive())
+					rc.yield();
+				for (Robot[] nearbyEnemies = rc.senseNearbyGameObjects(
+						Robot.class, 10, rc.getTeam().opponent()); nearbyEnemies.length > 0; nearbyEnemies = rc
+						.senseNearbyGameObjects(Robot.class, 10, rc.getTeam()
+								.opponent())) {
+					rc.attackSquare(rc.senseRobotInfo(nearbyEnemies[0]).location);
+					while (!rc.isActive())
+						rc.yield();
+				}
+				for (int i = dirTo + 8; i > dirTo; i--) {
+					if (rc.canMove(directions[i % 8]) && i % 8 != from) {
+						from = directions[i % 8].opposite().ordinal();
+						rc.move(directions[i % 8]);
+						break;
+					}
+				}
+				dirTo = rc.getLocation().directionTo(target).ordinal();
+			}
+			System.out.println("reached target");
+			rc.yield();
+		} catch (Exception e) {
+			System.out.println("Navigation Exception");
+			e.printStackTrace();
+		}
 	}
 
 	private static void runPastr() {
