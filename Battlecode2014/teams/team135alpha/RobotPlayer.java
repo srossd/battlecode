d5f0c8259ab5771e519d58b536d494716dee9cf4
Risheek Rajolu <risheekrr@gmail.com>
2014-01-31 15:21:54 -0500

    updated navigation: robots attack while navigating, robots target 4 corners, 4 robots per corner (pastr, 2 guards, noise), noise tower construction not working

--- Battlecode2014/teams/team135alpha/RobotPlayer.java
+++ Battlecode2014/teams/team135alpha/RobotPlayer.java
@@ -15,14 +15,20 @@ public class RobotPlayer {
 			Direction.SOUTH, Direction.SOUTH_WEST, Direction.WEST,
 			Direction.NORTH_WEST };
 
+	static boolean docked = true; // for soldiers docked to HQ
+
 	/* channel numbers */
 	static int releaseAttackChannel = 0;
 
-	/* TODO replace temp */
-	static int[] cornerChannels = new int[] { 1, 2, 3, 4 }; // corner pastr,
-															// guard1, guard2,
-															// noise
-	static MapLocation[] corners;
+	static int[] targetChannels = new int[] { 1, // pastr
+			2, // guard 1
+			3, // guard 2
+			4 // noise tower
+	};
+
+	static MapLocation[] targets;
+
+	static MapLocation target = null;
 
 	public static void run(RobotController rc) {
 
@@ -83,57 +89,40 @@ public class RobotPlayer {
 	}
 
 	private static void runSoldier() {
-		boolean docked = true;
 		while (true) {
 			try {
-				while (!rc.isActive())
-					rc.yield();
-				for (Robot[] nearbyEnemies = rc.senseNearbyGameObjects(
-						Robot.class, 10, rc.getTeam().opponent()); nearbyEnemies.length > 0; nearbyEnemies = rc
-						.senseNearbyGameObjects(Robot.class, 10, rc.getTeam()
-								.opponent())) {
-					rc.attackSquare(rc.senseRobotInfo(nearbyEnemies[0]).location);
-					while (!rc.isActive())
-						rc.yield();
-				}
-				int width = rc.getMapWidth();
-				int height = rc.getMapHeight();
-				corners = new MapLocation[] {
-						new MapLocation(width - 3, height - 3),
-						new MapLocation(width - 3, 2),
-						new MapLocation(2, height - 3), new MapLocation(2, 2) };
-				MapLocation center = new MapLocation(width / 2, height / 2);
-				for (int i = 0; i < 4; i++) {
-					TerrainTile tt = rc.senseTerrainTile(corners[i]);
-					if (tt != TerrainTile.NORMAL && tt != TerrainTile.ROAD)
-						corners[i] = corners[i].add(corners[i]
-								.directionTo(center));
-				}
 				if (docked) {
-					if (rc.readBroadcast(releaseAttackChannel) == 1) {
-						for (int i = 0; i < cornerChannels.length; i++) {
-							int corner = rc.readBroadcast(cornerChannels[i]);
-							if (corner < 4) {
-								rc.broadcast(cornerChannels[i], corner + 1);
-								navigate(corners[corner]);
-								if (i == 0) {
-									while (!rc.isActive())
-										rc.yield();
-									rc.construct(RobotType.PASTR);
-									while (true)
-										rc.yield();
-								} else if (i == 4) {
-									rc.construct(RobotType.NOISETOWER);
-									while (true)
-										rc.yield();
-								} else {
-
-								}
-
-								break;
+					while (rc.readBroadcast(releaseAttackChannel) == 0) {
+						while (!rc.isActive())
+							rc.yield();
+						attack();
+					}
+					docked = false;
+				}
+				for (int i = 0; i < targetChannels.length; i++) {
+					int t = rc.readBroadcast(targetChannels[i]);
+					if (t < 4) {
+						//System.out.println(i+" "+t);
+						rc.broadcast(targetChannels[i], t + 1);
+						setTarget(t);
+						navigate();
+						//System.out.println("done navigating");
+						if (i == 0) {
+							while (!rc.isActive())
+								rc.yield();
+							//rc.construct(RobotType.PASTR);
+							while (true)
+								rc.yield();
+						} else if (i == 4) {
+							//System.out.println("noisinngg");
+							rc.construct(RobotType.NOISETOWER);
+							while (true)
+								rc.yield();
+						} else {
+							while (true) {
+								rc.yield();
 							}
 						}
-						docked = false;
 					}
 				}
 
@@ -145,48 +134,93 @@ public class RobotPlayer {
 		}
 	}
 
-	private static void navigate(MapLocation target) {
-		System.out.println(target);
+	private static void setTarget(int t) {
+		try {
+			int width = rc.getMapWidth();
+			int height = rc.getMapHeight();
+			targets = new MapLocation[] {
+					new MapLocation(width - 3, height - 3),
+					new MapLocation(width - 3, 2),
+					new MapLocation(2, height - 3), new MapLocation(2, 2) };
+			MapLocation center = new MapLocation(width / 2, height / 2);
+			for (int i = 0; i < 4; i++) {
+
+				for (TerrainTile tt = rc.senseTerrainTile(targets[i]); tt != TerrainTile.NORMAL
+						&& tt != TerrainTile.ROAD; tt = rc
+						.senseTerrainTile(targets[i]))
+					targets[i] = targets[i].add(targets[i].directionTo(center));
+			}
+			target = targets[t];
+		} catch (Exception e) {
+			System.out.println("Targeting Exception");
+			e.printStackTrace();
+		}
+	}
+
+	private static void navigate() {
+		//System.out.println(target);
 		try {
 			int dirTo = rc.getLocation().directionTo(target).ordinal();
 			int from = rc.getLocation().directionTo(rc.senseHQLocation())
 					.ordinal();
-			for (int distTo = rc.getLocation().distanceSquaredTo(target); distTo > 1
-					|| distTo == 1 && rc.canMove(directions[dirTo]); distTo = rc
+			for (int distTo = rc.getLocation().distanceSquaredTo(target); distTo > 2
+					|| distTo > 0 && rc.canMove(directions[dirTo]); distTo = rc
 					.getLocation().distanceSquaredTo(target)) {
-				while (!rc.isActive())
-					rc.yield();
-				for (Robot[] nearbyEnemies = rc.senseNearbyGameObjects(
-						Robot.class, 10, rc.getTeam().opponent()); nearbyEnemies.length > 0; nearbyEnemies = rc
-						.senseNearbyGameObjects(Robot.class, 10, rc.getTeam()
-								.opponent())) {
-					rc.attackSquare(rc.senseRobotInfo(nearbyEnemies[0]).location);
-					while (!rc.isActive())
-						rc.yield();
-				}
+				attack();
 				for (int i = dirTo + 8; i > dirTo; i--) {
 					if (rc.canMove(directions[i % 8]) && i % 8 != from) {
 						from = directions[i % 8].opposite().ordinal();
 						rc.move(directions[i % 8]);
+						while (!rc.isActive())
+							rc.yield();
 						break;
 					}
 				}
 				dirTo = rc.getLocation().directionTo(target).ordinal();
 			}
-			System.out.println("reached target");
-			rc.yield();
+			//System.out.println("reached target");
 		} catch (Exception e) {
 			System.out.println("Navigation Exception");
 			e.printStackTrace();
 		}
 	}
 
-	private static void runPastr() {
+	private static void attack() {
+		try {
+			while (!rc.isActive())
+				rc.yield();
+			for (Robot[] nearbyEnemies = rc.senseNearbyGameObjects(Robot.class,
+					10, rc.getTeam().opponent()); nearbyEnemies.length > 0; nearbyEnemies = rc
+					.senseNearbyGameObjects(Robot.class, 10, rc.getTeam()
+							.opponent())) {
+				rc.attackSquare(rc.senseRobotInfo(nearbyEnemies[0]).location);
+				while (!rc.isActive())
+					rc.yield();
+			}
+		} catch (Exception e) {
+			System.out.println("Attacking Exception");
+			e.printStackTrace();
+		}
+	}
 
+	private static void runPastr() {
+		try {
+			while (true)
+				rc.yield();
+		} catch (Exception e) {
+			System.out.println("PASTR Exception");
+			e.printStackTrace();
+		}
 	}
 
 	private static void runNoisetower() {
-
+		try {
+			while (true)
+				rc.yield();
+		} catch (Exception e) {
+			System.out.println("PASTR Exception");
+			e.printStackTrace();
+		}
 	}
 
 }
